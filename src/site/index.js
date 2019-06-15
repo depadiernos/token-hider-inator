567945151d7e94e575080331375913790df0f54b
Dennis Padiernos <depadiernos@gmail.com>
2019-06-14 22:19:44 -0500

    make embedded map larger

--- src/site/index.js
+++ src/site/index.js
@@ -14,7 +14,7 @@ const api = {
     const coordinates = document.getElementById("coordinates");
     const map = document.getElementById("map");
     console.log(JSON.stringify(result));
-    const url = `<iframe width='100%' height='100%' id='mapcanvas' src='https://maps.google.com/maps?q=${lat}+${lng}&z=10&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'><div class='zxos8_gm'><a rel='nofollow' href='https://themesort.com/category/agency-themes'>Agency templates at themesort</a></div><div style='overflow:hidden;'><div id='gmap_canvas' style='height:100%;width:700px;'></div><div><small>Powered by <a href='https://www.embedgooglemap.co.uk'>Embed Google Map</a></small></div></iframe>`;
+    const url = `<iframe width='100%' height='100%' id='mapcanvas' src='https://maps.google.com/maps?q=${lat}+${lng}&z=10&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'><div class='zxos8_gm'><a rel='nofollow' href='https://themesort.com/category/agency-themes'>Agency templates at themesort</a></div><div style='overflow:hidden;'><div id='gmap_canvas' style='height:100%;width:800px;'></div><div><small>Powered by <a href='https://www.embedgooglemap.co.uk'>Embed Google Map</a></small></div></iframe>`;
     map.innerHTML = url;
     map.hidden = false;
   },
