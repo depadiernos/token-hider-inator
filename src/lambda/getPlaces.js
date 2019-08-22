c8acc34c4b696c2c2f8493afeb17524aed8074f0
Dennis Padiernos <depadiernos@gmail.com>
2019-08-22 06:57:20 -0600

    migrate site to react, refactor getPlaces and use netlify dev

--- src/lambda/getPlaces.js
+++ src/lambda/getPlaces.js
@@ -1,40 +1,40 @@
-const qs = require("qs")
-const geoip = require('geoip-lite')
-const { API_TOKEN } = process.env
-const googleMapsClient = require('@google/maps').createClient({
-  key: API_TOKEN,
-  Promise: Promise
-});
 
-exports.handler = function(event, context, callback) {
-  
-  const query = qs.stringify(event.queryStringParameters)
+exports.handler = function (event, context, callback) {
+  const qs = require("qs")
+  const geoip = require('geoip-lite')
+  const params = event.queryStringParameters
   const ipaddress = event.headers["client-ip"]
-  const {ll} = geoip.lookup(ipaddress)
+  const location = geoip.lookup(ipaddress)
+  const ll = (location)? location.ll: null
+  const { API_TOKEN } = process.env
+  const googleMapsClient = require('@google/maps').createClient({
+    key: API_TOKEN,
+    Promise: Promise
+  });
 
   // Let's log some stuff we already have.
   console.log("logging event.....", event)
-  console.log("logging params ", query, event.queryStringParameters)
+  console.log("logging params ", params)
   console.log("logging client's ip address ", ipaddress)
 
-   // Here's a function we'll use to define how our response will look like when we call callback
-  const pass = (body) => {callback( null, {
-    statusCode: 200,
-    body: JSON.stringify(body)
-  })}
+  // Here's a function we'll use to define how our response will look like when we call callback
+  const pass = (body) => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(body)
+    })
+  }
 
   // Perform the Places Query call.
   const get = () => {
-    googleMapsClient.places({query: query, location: ll}).asPromise()
-    .then((response) =>
-      {
-        console.log(response.json.results)
+    googleMapsClient.places({ query: params.query, location: ll }).asPromise()
+      .then((response) => {
         pass(response.json.results)
       }
-    )
-    .catch(err => pass(err))
+      )
+      .catch(err => pass(err))
   }
-  if(event.httpMethod == 'GET'){
+  if (event.httpMethod == 'GET') {
     get()
   };
 };
--- src/site/index.js
+++ src/site/index.js
@@ -1,8 +1,11 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+
 const api = {
   encode: () => {
-    const address = document.getElementById("address");
     const encoded = address.value.split(" ").join("+");
-    const params = "?address=" + encoded;
+    const params = "?query=" + encoded;
     return params;
   },
   send: () => {
@@ -14,7 +17,7 @@ const api = {
     const coordinates = document.getElementById("coordinates");
     const map = document.getElementById("map");
     console.log(JSON.stringify(result));
-    const url = `<iframe width='100%' height='400px' id='mapcanvas' src='https://maps.google.com/maps?q=${lat}+${lng}&z=16&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'><div class='zxos8_gm'><a rel='nofollow' href='https://themesort.com/category/agency-themes'>Agency templates at themesort</a></div><div style='overflow:hidden;'><div id='gmap_canvas' style='height:100%;width:100%;'></div><div><small>Powered by <a href='https://www.embedgooglemap.co.uk'>Embed Google Map</a></small></div></iframe>`;
+    const url = `<iframe width='100%' height='400px' id='mapcanvas' src='https://maps.google.com/maps?q=place_id${placeId}&z=16&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'><div class='zxos8_gm'><a rel='nofollow' href='https://themesort.com/category/agency-themes'>Agency templates at themesort</a></div><div style='overflow:hidden;'><div id='gmap_canvas' style='height:100%;width:100%;'></div><div><small>Powered by <a href='https://www.embedgooglemap.co.uk'>Embed Google Map</a></small></div></iframe>`;
     map.innerHTML = url;
     map.hidden = false;
   },
@@ -27,15 +30,51 @@ const api = {
 };
 
 //Create eventlistener
-const submit = document.getElementById("submit");
-const input = document.getElementById("address");
-submit.addEventListener("click", api.send, false);
-input.addEventListener(
-  "keyup",
-  function(event) {
-    if (event.key == "Enter") {
-      api.send();
-    }
-  },
-  false
-);
+// const submit = document.getElementById("submit");
+// const input = document.getElementById("address");
+// submit.addEventListener("click", api.send, false);
+// input.addEventListener(
+//   "keyup",
+//   function (event) {
+//     if (event.key == "Enter") {
+//       api.send();
+//     }
+//   },
+//   false
+// );
+
+function App() {
+  const [query, setQuery] = useState('')
+  return (
+    <div className="modal">
+      <header>
+        <h1>Hazards of Maps!</h1>
+        <p>
+          This site will call a lambda function, which will call an api
+          endpoint, inject my Google Map API key and pass back the response in
+          JSON format.
+  </p>
+      </header>
+
+      <div className="form">
+        <h4>Please enter your address:</h4>
+        <input
+          id="address"
+          type="text"
+          placeholder="street number and name, city, state"
+        />
+        <button id="submit">Submit</button>
+      </div>
+      <div id="coordinates" hidden>
+        The coordinates for the address you entered is
+  <span id="coordindate-value"></span>
+      </div>
+      <div id="map" hidden>
+        Click the following link to view in Google Maps<br />
+        <span id="map-link"></span>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, document.querySelector("#app"))
