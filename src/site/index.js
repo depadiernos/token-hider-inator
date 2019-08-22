39924f838fa77564f08af689d92d70c270a3c666
Dennis Padiernos <depadiernos@gmail.com>
2019-08-22 11:56:40 -0600

    get things running

--- src/site/index.js
+++ src/site/index.js
@@ -1,80 +1,56 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
-import styled from 'styled-components'
+import React, { useState } from "react"
+import ReactDOM from "react-dom"
 
-const api = {
-  encode: () => {
-    const encoded = address.value.split(" ").join("+");
-    const params = "?query=" + encoded;
-    return params;
-  },
-  send: () => {
-    encode = api.encode;
-    api.get(encode);
-  },
-  show: result => {
-    const { lat, lng } = result[0].geometry.location;
-    const coordinates = document.getElementById("coordinates");
-    const map = document.getElementById("map");
-    console.log(JSON.stringify(result));
-    const url = `<iframe width='100%' height='400px' id='mapcanvas' src='https://maps.google.com/maps?q=place_id${placeId}&z=16&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'><div class='zxos8_gm'><a rel='nofollow' href='https://themesort.com/category/agency-themes'>Agency templates at themesort</a></div><div style='overflow:hidden;'><div id='gmap_canvas' style='height:100%;width:100%;'></div><div><small>Powered by <a href='https://www.embedgooglemap.co.uk'>Embed Google Map</a></small></div></iframe>`;
-    map.innerHTML = url;
-    map.hidden = false;
-  },
-  get: params => {
-    fetch("/.netlify/functions/getapi" + api.encode())
-      .then(response => response.json())
-      .then(result => api.show(result.results))
-      .catch(err => console.log(err));
-  }
-};
-
-//Create eventlistener
-// const submit = document.getElementById("submit");
-// const input = document.getElementById("address");
-// submit.addEventListener("click", api.send, false);
-// input.addEventListener(
-//   "keyup",
-//   function (event) {
-//     if (event.key == "Enter") {
-//       api.send();
-//     }
-//   },
-//   false
-// );
+const encode = () => {
+  const encoded = address.value.split(" ").join("+")
+  const params = "?query=" + encoded
+  return params
+}
 
 function App() {
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState("")
+  const [places, setPlaces] = useState([])
+  const getPlaces = (event) => {
+    event.preventDefault()
+    fetch("/.netlify/functions/getPlaces" + encode(query))
+      .then((response) => setPlaces(response.body))
+      .catch((err) => console.log(err))
+  }
+
   return (
     <div className="modal">
       <header>
         <h1>Hazards of Maps!</h1>
         <p>
           This site will call a lambda function, which will call an api
-          endpoint, inject my Google Map API key and pass back the response in
-          JSON format.
-  </p>
+          endpoint, inject my Google Map API key and pass back the response in JSON.
+        </p>
       </header>
 
       <div className="form">
-        <h4>Please enter your address:</h4>
+        <h4>Search for a place or type of place nearby:</h4>
         <input
-          id="address"
+          id="query"
           type="text"
-          placeholder="street number and name, city, state"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="e.g. food, store, etc."
         />
-        <button id="submit">Submit</button>
+        <button onClick={getPlaces} id="submit">
+          Submit
+        </button>
       </div>
-      <div id="coordinates" hidden>
-        The coordinates for the address you entered is
-  <span id="coordindate-value"></span>
-      </div>
-      <div id="map" hidden>
-        Click the following link to view in Google Maps<br />
-        <span id="map-link"></span>
-      </div>
+
+      {
+        if (places){
+          places.map((place) => {
+            return <div>{place.name}</div>
+          })
+        }
+      }
+
     </div>
   )
 }
 
-ReactDOM.render(<App/>, document.querySelector("#app"))
+ReactDOM.render(<App />, document.querySelector("#app"))
