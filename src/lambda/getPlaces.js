d3d533ee30fd00df6776ce09274332a7f6874919
Dennis Padiernos <depadiernos@gmail.com>
2019-08-21 23:03:56 -0600

    add geoip location to query

--- src/lambda/getPlaces.js
+++ src/lambda/getPlaces.js
@@ -1,4 +1,5 @@
 const qs = require("qs")
+const geoip = require('geoip-lite')
 const { API_TOKEN } = process.env
 const googleMapsClient = require('@google/maps').createClient({
   key: API_TOKEN,
@@ -8,10 +9,13 @@ const googleMapsClient = require('@google/maps').createClient({
 exports.handler = function(event, context, callback) {
   
   const query = qs.stringify(event.queryStringParameters)
+  const ipaddress = event.headers["client-ip"]
+  const {ll} = geoip.lookup(ipaddress)
 
   // Let's log some stuff we already have.
   console.log("logging event.....", event)
-  console.log("logging params", query, event.queryStringParameters)
+  console.log("logging params ", query, event.queryStringParameters)
+  console.log("logging client's ip address ", ipaddress)
 
    // Here's a function we'll use to define how our response will look like when we call callback
   const pass = (body) => {callback( null, {
@@ -21,7 +25,7 @@ exports.handler = function(event, context, callback) {
 
   // Perform the Places Query call.
   const get = () => {
-    googleMapsClient.places({query: 'food'}).asPromise()
+    googleMapsClient.places({query: 'food', location: ll}).asPromise()
     .then((response) =>
       {
         console.log(response.json.results)
